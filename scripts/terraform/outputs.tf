25a5964e5551aa49100dd6d65ca487b4d4d91577
Guillermo Valin <contacto@guillermovalin.com>
2026-01-23 12:51:10 -0500

    add nat gateway to terraform

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -50,6 +50,20 @@ output "internet_gateway_id" {
 }
 
 # -----------------------------------------------------------------------------
+# NAT Gateway Outputs
+# -----------------------------------------------------------------------------
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.main.id
+}
+
+output "nat_gateway_public_ip" {
+  description = "Public IP address of the NAT Gateway"
+  value       = aws_eip.nat.public_ip
+}
+
+# -----------------------------------------------------------------------------
 # Security Group Outputs
 # -----------------------------------------------------------------------------
 
